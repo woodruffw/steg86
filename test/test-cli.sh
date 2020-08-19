fcd36bd9dfe282ca011196cdd0274d650b79f3e1
William Woodruff <william@yossarian.net>
2020-08-18 20:51:15 -0400

    test-cli: Add raw roundtrip test

--- test/test-cli.sh
+++ test/test-cli.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+TEST_DEPS=(shasum objdump)
 FIXTURE="${FIXTURE:-/bin/bash}"
 
 function steg86 {
@@ -9,6 +10,13 @@ function steg86 {
 function die {
   >&2 echo "Fatal: ${*}"
   exit 1
+}
+
+function installed {
+  cmd=$(command -v "${1}")
+
+  [[ -n "${cmd}" ]] && [[ -f "${cmd}" ]]
+  return ${?}
 }
 
 function test_steg86_profile_has_consistent_output {
@@ -45,9 +53,41 @@ function test_steg86_embed_extract_roundtrip {
   echo "OK"
 }
 
+function test_steg86_embed_extract_roundtrip_raw {
+  echo -n "${FUNCNAME[0]} "
+
+  raw_text=$(mktemp -u /tmp/steg86-XXXXX)
+  objcopy "${FIXTURE}" /dev/null --dump-section .text="${raw_text}"
+
+  embedded=$(mktemp /tmp/steg86-XXXXX)
+  head -c 1024 < /dev/urandom > "${embedded}"
+
+  stegdest=$(mktemp -u /tmp/steg86-XXXXX)
+  steg86 embed --raw "${raw_text}" "${stegdest}" < "${embedded}"
+
+  extracted=$(mktemp /tmp/steg86-XXXXX)
+  steg86 extract --raw "${stegdest}" > "${extracted}"
+
+  embedded_cksum=$(shasum < "${embedded}")
+  extracted_cksum=$(shasum < "${extracted}")
+
+  rm -f "${raw_text}" "${embedded}" "${stegdest}" "${extracted}"
+
+  [[ "${embedded_cksum}" = "${extracted_cksum}" ]] \
+    || die "steg86 raw embed/extract does not roundtrip correctly"
+
+  echo "OK"
+}
+
+for dep in "${TEST_DEPS[@]}"; do
+  installed "${dep}" || die "Missing test dependency: ${dep}"
+done
+
 [[ -f "${FIXTURE}" ]] || die "missing fixture to test with: ${FIXTURE}"
+[[ "$(uname -m)" = "x86_64" ]] || die "CLI tests must be run on x86_64"
 
 test_steg86_profile_has_consistent_output
 test_steg86_embed_extract_roundtrip
+test_steg86_embed_extract_roundtrip_raw
 
 exit 0
